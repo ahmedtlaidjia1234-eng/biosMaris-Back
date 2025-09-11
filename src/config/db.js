23ccd11c0834ffca7c7969b76ac8c0e92e00d063
ahmedTL <ahmedtlaidjia1234@gmail.com>
2025-09-11 01:50:26 +0100

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,17 +3,24 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
+const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT, 
-    logging: false, // set true to see raw SQL logs
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
+
 
 export const connectDB = async () => {
   try {
